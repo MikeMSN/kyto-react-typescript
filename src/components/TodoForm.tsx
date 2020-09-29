04745105edafcdce6940d0907ab45915ab818210
17543268 <Mosin.M.Yu@sberbank.ru>
2020-09-30 01:16:13 +0300

    added handler for input

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const TodoForm: React.FC = () => {
+
+    const [title, setTitle] = useState<string>('');
+
+    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.target.value);
+    }
+
+    const keyPressHandler = (event: React.KeyboardEvent) => {
+        if(event.key === 'Enter') {
+            console.log(title);
+            setTitle('');
+        }
+    }
+
     return (
         <div className="input-field mt2">
-            <input type="text" id="title" placeholder="Введите название дела"/>
+            <input
+                onChange={changeHandler}
+                    value={title}
+                   type="text" id="title" placeholder="Введите название дела"
+                    onKeyPress={keyPressHandler}
+            />
             <label htmlFor="title" className="active">Введите название дела</label>
         </div>
     )
